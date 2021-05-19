aa056e0b62ca0c18e33d64dbbd587827a106b83d
Arun Subramaniam <s.arunsubramaniam@gmail.com>
2021-05-20 00:53:21 +0530

    UI Changes 1. Card appearance in docs 2. Tab, Accordion CSS 3. No class name for undefined

--- src/components/Common/Card.jsx
+++ src/components/Common/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className={`grid-wrapper card ${props.className}`}>
+    <div className={`grid-wrapper card ${props.className !== undefined ? props.className : ''}`}>
       <div className="grid-12 card-content">{props.children}</div>
     </div>
   );
