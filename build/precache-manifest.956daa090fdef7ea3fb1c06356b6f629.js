36bdb739f57fceffd5cd1e948f12d92b2d1a1b79
Arun Subramaniam <s.arunsubramaniam@gmail.com>
2023-03-09 15:28:45 +0530

    Build File

---
+++ build/precache-manifest.956daa090fdef7ea3fb1c06356b6f629.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038b2a29250b7d75d43cc405f7be891e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "67a9d22de337a66b642c",
+    "url": "/react-gh-pages/static/css/main.c50d996e.chunk.css"
+  },
+  {
+    "revision": "4ff3112ca89ed12a89dc",
+    "url": "/react-gh-pages/static/js/2.fe2ef16a.chunk.js"
+  },
+  {
+    "revision": "c90c8ae3244f49612816aa7cab780b18",
+    "url": "/react-gh-pages/static/js/2.fe2ef16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a9d22de337a66b642c",
+    "url": "/react-gh-pages/static/js/main.cfbf6384.chunk.js"
+  },
+  {
+    "revision": "989939337d6c3c0f2370",
+    "url": "/react-gh-pages/static/js/runtime-main.9d71cbe7.js"
+  },
+  {
+    "revision": "6947316e1947dbb9255b6144df0626da",
+    "url": "/react-gh-pages/static/media/components.6947316e.svg"
+  },
+  {
+    "revision": "a1d6b5e2efeb8df11c1890c28309f81f",
+    "url": "/react-gh-pages/static/media/cube.a1d6b5e2.svg"
+  },
+  {
+    "revision": "a402d19a7f8eb15301cbefaac86b2779",
+    "url": "/react-gh-pages/static/media/elegance.a402d19a.svg"
+  },
+  {
+    "revision": "c63758cf2b592b7b20e61d84ec48dd58",
+    "url": "/react-gh-pages/static/media/elements.c63758cf.svg"
+  },
+  {
+    "revision": "15c9a717574370c35311bd6f13123acd",
+    "url": "/react-gh-pages/static/media/form.15c9a717.svg"
+  },
+  {
+    "revision": "c450c9b8437a861882cf18f72adb8afc",
+    "url": "/react-gh-pages/static/media/getStarted.c450c9b8.svg"
+  },
+  {
+    "revision": "0bde061017a014890a1fbcc39ed6af1d",
+    "url": "/react-gh-pages/static/media/git.0bde0610.svg"
+  },
+  {
+    "revision": "9a405fa7bd257fc02bd5ce75dab9dcab",
+    "url": "/react-gh-pages/static/media/helpers.9a405fa7.svg"
+  },
+  {
+    "revision": "26b84142c0a3c83338c3bab2fc7e0441",
+    "url": "/react-gh-pages/static/media/icon.26b84142.svg"
+  },
+  {
+    "revision": "610738b20a79b503820cbf06e6cf66b7",
+    "url": "/react-gh-pages/static/media/jQuery.610738b2.svg"
+  },
+  {
+    "revision": "3714b5e205db00eeee1ff9f4efe01d88",
+    "url": "/react-gh-pages/static/media/layout.3714b5e2.svg"
+  },
+  {
+    "revision": "d165bea618098988405643d769e0e2ff",
+    "url": "/react-gh-pages/static/media/logo.d165bea6.svg"
+  },
+  {
+    "revision": "141fcd3a9284fb5303e32032fe757d13",
+    "url": "/react-gh-pages/static/media/responsive.141fcd3a.svg"
+  },
+  {
+    "revision": "77758bede493a0ac09d97c6a030a118f",
+    "url": "/react-gh-pages/static/media/sass.77758bed.svg"
+  }
+]);
